95db4f7b824348941ee9a8a3b9c8191ac4c5eb10
Santosh <santosh.mutyala.dev@gmail.com>
2026-01-21 11:27:20 +0100

    feat: create database module index
    - Create database/index.ts to export all modules
    - Re-export types, client, and all operation modules
    - Provide clean public API for database operations

---
+++ mimir-rag/src/database/index.ts
@@ -0,0 +1,7 @@
+export * from "./types";
+export * from "./client";
+export * from "./chunks";
+export * from "./moves";
+export * from "./orphans";
+export * from "./search";
+export * from "./prisma";
